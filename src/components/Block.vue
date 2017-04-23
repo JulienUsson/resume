35131b36eda21cf27bb32aa88d6f50c9ef310c07
Julien USSON <usson.julien@gmail.com>
2017-04-24 00:02:42 +0200

    Education block

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="block">
+    <h2>{{title}}</h2>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+  ],
+};
+</script>
+
+<style lang="scss">
+  .block {
+    width: 90%;
+    margin: 10px auto;
+     h2 {
+       padding-bottom: 10px;
+     }
+  }
+</style>
--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="education">
-
-  </div>
+  <block id="education" title="Formation">
+    <timeline>
+      <timeline-item v-for="education in educations">
+        <b>{{education.date}}</b> - {{education.title}}
+      </timeline-item>
+    </timeline>
+  </block>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { Timeline, TimelineTitle, TimelineItem } from './Timeline';
+import Block from './Block';
 
 export default {
   computed: {
     ...mapGetters([
       'educations',
     ]),
+  },
+  components: {
+    Block,
+    Timeline,
+    TimelineTitle,
+    TimelineItem,
   },
 };
 </script>
