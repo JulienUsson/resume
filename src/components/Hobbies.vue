646e2cec7e1ead5584d6fa41cafb76813310dbb4
Julien USSON <usson.julien@gmail.com>
2017-04-24 14:02:48 +0200

    Hobbies

--- src/components/Hobbies.vue
+++ src/components/Hobbies.vue
@@ -1,12 +1,17 @@
 <template>
-  <block id="hobbies" title="Centres d'intérêt">
-  
+  <block title="Centres d'intérêt">
+    <div id="hobbies">
+      <template v-for="hobby in hobbies">
+        <list-item><span class="title">{{hobby.title}}</span> <span class="description">{{hobby.description}}</span></list-item>
+      </template>
+    </div>
   </block>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import Block from './Block';
+import ListItem from './ListItem';
 
 export default {
   computed: {
@@ -16,10 +21,18 @@ export default {
   },
   components: {
     Block,
+    ListItem,
   },
 };
 </script>
 
 <style>
+#hobbies .title {
+  font-weight: bold;
+  font-size: 1em;
+}
 
+#hobbies .description {
+  font-size: 0.9em;
+}
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="list-item">
+      <svg className="line" width="20" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <ellipse cx="10" cy="10" rx="10" ry="10" />
+      </svg>
+      <p>
+        <slot/>
+      </p>
+    </div>
+    <timeline-separator/>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+
+.list-item {
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  margin-bottom: 30px;
+
+  svg {
+    position: absolute;
+    height: 100%;
+
+    ellipse {
+      fill: $accent-color;
+    }
+  }
+
+  p {
+    margin-left: 30px;
+    line-height: 24px;
+  }
+}
+</style>
